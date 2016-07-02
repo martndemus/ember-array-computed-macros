5c4d8a1d6306714ed7651cdab94275b10fa78632
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2016-07-03 01:28:57 +0300

    Failing test for #5: meanBy returns NAN when listProperty is a relationship

--- tests/unit/mean-test.js
+++ tests/unit/mean-test.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
-import { module, test } from 'qunit';
+
+const {
+  run
+} = Ember;
+
+import { moduleForModel, test } from 'ember-qunit';
 
 import { meanBy } from 'ember-array-computed-macros';
 
-module('Unit | Computed | meanBy');
+
+moduleForModel('parent', 'Unit | Computed | meanBy', {
+  // Specify the other units that are required for this test.
+  needs: ['model:child']
+});
 
 test('Sums a list of values', (assert) => {
   const subject = Ember.Object.extend({
@@ -11,4 +20,19 @@ test('Sums a list of values', (assert) => {
   }).create({ list: [{ score: 1 }, { score: 10 }, { score: 4 }] });
 
   assert.equal(subject.get('meanValue'), 5);
+});
+
+
+test('Sums a list of values from async relationship', function (assert) {
+  run(() => {
+    const store  = this.store();
+    const parent = this.subject({
+      children: [
+        store.createRecord('child', {age: 2}),
+        store.createRecord('child', {age: 4})
+      ]
+    });
+
+    assert.equal(parent.get('meanChildAge'), 3);
+  });
 });
